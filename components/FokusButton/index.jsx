1eda9b9ee745bbc7a25635ec77b95e6d9624d48a
Vinicius <viniciusr.camargo@gmail.com>
2025-07-05 19:22:05 -0300

    Finalizando o projeto

--- components/FokusButton/index.jsx
+++ components/FokusButton/index.jsx
@@ -1,10 +1,11 @@
 import {Pressable, Text, StyleSheet} from 'react-native';
 
-export default function FokusButton({onPress}) {
+export default function FokusButton({onPress, title, icon}) {
     return(
         <Pressable style={styles.button} onPress={onPress}>
+          {icon}
           <Text style={styles.buttonText}>
-            Pressionar
+            {title}
           </Text>
           </Pressable>
     )
@@ -15,7 +16,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#B872FF',
     borderRadius: 32,
     padding: 10,
-  },
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+    justifyContent: 'center'},
   buttonText:{
     textAlign: 'center',
     color: '#021123',
