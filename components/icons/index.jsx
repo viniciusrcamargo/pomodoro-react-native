f06571c5c84927830617d3992517279bbd4ee8cd
Vinicius <viniciusr.camargo@gmail.com>
2025-07-08 19:24:13 -0300

    Finalizando o projeto

---
+++ components/icons/index.jsx
@@ -0,0 +1,19 @@
+import Svg, { Path } from 'react-native-svg';
+
+
+export const IconPlay = () => {
+    return (
+        <Svg width="11" height="14" viewBox="0 0 11 14" fill="none">
+            <Path d="M0.015625 0.015625L10.9844 7L0.015625 13.9844V0.015625Z" fill="white" />
+        </Svg>
+    )
+}
+
+export const IconPause = () => {
+    return (
+        <Svg width="12" height="14" viewBox="0 0 12 14" fill="none">
+            <Path d="M8.01562 0.015625H12V13.9844H8.01562V0.015625ZM0 13.9844V0.015625H3.98438V13.9844H0Z" fill="white" />
+        </Svg>
+
+    )
+}
